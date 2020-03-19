0fd8d555b97574e6467cc5b202689cb48c1fb89f
Tok1tz <52502935+Tok1tz@users.noreply.github.com>
2020-03-18 23:52:57 -0300

    Changed the time when the bot posts tweets.

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ auth = tweepy.OAuthHandler(TWIITER_CONSUMER_KEY, TWIITER_CONSUMER_SECRET)
 auth.set_access_token(TWIITER_ACCESS_TOKEN, TWIITER_ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+ASAP_TIME= "12:30"
 ASTRONAUTS= "13:30"
-ASAP_TIME= "11:30"
-TOP_TIME= "17:00"
-NEW_TIME= "22:00"
-SEGUNDOS = 5
+TOP_TIME_1= "15:00"
+TOP_TIME_2= "16:00"
+TOP_TIME_3= "17:00"
+NEW_TIME_1= "18:00"
+NEW_TIME_2= "22:00"
+NEW_TIME_3= "00:00"
+MINUTOS = 15
 
+#Se declaran las tareas diarias a ejecutar
 schedule.every().day.at(ASTRONAUTS).do(astronauts.Post, api)
 schedule.every().day.at(ASAP_TIME).do(nasaAPI.Post, api)
-schedule.every().day.at(TOP_TIME).do(redditAPI.Top_post, api)
-schedule.every().day.at(NEW_TIME).do(redditAPI.New_post, api)
+schedule.every().day.at(TOP_TIME_1).do(redditAPI.Top_post, api)
+schedule.every().day.at(TOP_TIME_2).do(redditAPI.Top_post, api)
+schedule.every().day.at(TOP_TIME_3).do(redditAPI.Top_post, api)
+schedule.every().day.at(NEW_TIME_1).do(redditAPI.New_post, api)
+schedule.every().day.at(NEW_TIME_2).do(redditAPI.New_post, api)
+schedule.every().day.at(NEW_TIME_3).do(redditAPI.New_post, api)
 
 print("[" + getTime() + "] Empiezan a ejecutarse las tareas...")
 while(True):
     schedule.run_pending()
-    print("[" + getTime() + "] La ejecución se para por " + str(SEGUNDOS) + " segundos.")
-    time.sleep(SEGUNDOS) #Evita la ejecucion constante del bucle
+    print("[" + getTime() + "] La ejecución se para por " + str(MINUTOS) + " minutos.")
+    time.sleep(MINUTOS * 60) #Evita la ejecucion constante del bucle
